061dc62dfbf0edce6fe5f821b6a0f050b2100f25
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2018-12-19 01:05:02 -0800

    12/18/2018

---
+++ Strings roman_to_integer.py
@@ -0,0 +1,15 @@
+
+import functools
+
+input = 'LIX'
+
+
+def roman_to_integer(s):
+    T = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+
+    return functools.reduce(lambda val, i: val + (-T[s[i]] if T[s[i]] < T[s[i + 1]] else T[s[i]]), reversed(range(len(s) - 1)), T[s[-1]])
+
+
+result = roman_to_integer(input)
+
+print(result)
