271b4014019e84050c997591996605d26f225983
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-11 16:19:40 -0800

    01/11/2019

---
+++ Stacks_1.py
@@ -0,0 +1,50 @@
+
+
+import collections
+
+
+class stack:
+    ElementWithCachedMax = collections.namedtuple('ElementWithCachedMax', ('element', 'max'))
+
+    def __init__(self):
+        self._element_with_cached_max = []
+
+    def empty(self):
+        return len(self._element_with_cached_max) == 0
+
+    def max(self):
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._element_with_cached_max[-1].max
+
+    def pop(self):
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        return self._element_with_cached_max.pop().element
+
+    def push(self, x):
+        self._element_with_cached_max.append(self.ElementWithCachedMax(x, x if self.empty() else max(x, self.max())))
+
+
+
+s1 = stack()
+
+s1.push(3)
+s1.push(4)
+s1.push(5)
+s1.push(6)
+s1.push(7)
+
+
+
+print(s1.pop())
+
+print(s1.empty())
+
+print(s1.max())
+
+
+
+
+
+
