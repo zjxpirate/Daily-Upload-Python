d4a2717208f1bd9bd403e773cd2a76ba220d7357
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-16 11:54:25 -0800

    01/16/2019

---
+++ Binary_Trees_node.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+from Binary_Trees_utils import binary_tree_to_string, equal_binary_trees
+
+
+
+class BinaryTreeNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+
+
+    def __eq__(self, other):
+        return equal_binary_trees(self, other)
+
+    def __repr__(self):
+        return str(binary_tree_to_string(self))
+
+    def __str__(self):
+        return self.__repr__()
+
+
+
+
+
+
+
+
+b = BinaryTreeNode(1, BinaryTreeNode(2, BinaryTreeNode(5)), BinaryTreeNode(3))
+
+
+
+#print(b)
+
+
+
+#print(b.left.right)
+#print(b.right)
