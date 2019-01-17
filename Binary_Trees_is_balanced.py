5dcc951dde9a9922ca40768472928f900a7898e6
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-16 17:24:31 -0800

    01/16/2019

---
+++ Binary_Trees_is_balanced.py
@@ -0,0 +1,43 @@
+
+
+import collections
+from Binary_Trees_node import BinaryTreeNode
+
+def is_balanced_binary_tree(tree):
+    BalancedStatusWithHeight = collections.namedtuple('BalancedStatusWithHeight', ('balanced', 'height'))
+
+
+    # First value of the return value indicates if tree is balanced, and if
+    # balanced the second value of the return value id the height of the tree.
+
+    def check_balanced(tree):
+        if not tree:
+            return BalancedStatusWithHeight(True, -1)  # Base case.
+
+        left_result = check_balanced(tree.left)
+        if not left_result.balanced:
+            # Left subtree is not balanced.
+            return BalancedStatusWithHeight(False, 0)
+
+
+        right_result = check_balanced(tree.right)
+        if not right_result.balanced:
+            # Right subtree is not balanced.
+            return BalancedStatusWithHeight(False, 0)
+
+
+        is_balanced = abs(left_result.height - right_result.height) <= 1
+        height = max(left_result.height, right_result.height) + 1
+        return BalancedStatusWithHeight(is_balanced, height)
+
+
+    return check_balanced(tree).balanced
+
+
+
+
+
+b = BinaryTreeNode(1, BinaryTreeNode(2, BinaryTreeNode(4), BinaryTreeNode(5)), BinaryTreeNode(3, BinaryTreeNode(6), BinaryTreeNode(7)))
+
+print(is_balanced_binary_tree(b))
+
