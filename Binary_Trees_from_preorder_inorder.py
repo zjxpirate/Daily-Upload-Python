f0c25341a92524e26b2f46707debafd422c5790d
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-01-21 11:16:49 -0800

    01/21/2019

--- Binary_Trees_from_preorder_inorder.py
+++ Binary_Trees_from_preorder_inorder.py
@@ -3,9 +3,6 @@
 
 
 
-
-
-
 #b = BinaryTreeNode(314, BinaryTreeNode(6 , BinaryTreeNode(271, BinaryTreeNode(28), BinaryTreeNode(0)), BinaryTreeNode(561, BinaryTreeNode(None), BinaryTreeNode(3, BinaryTreeNode(17)))), BinaryTreeNode(6, BinaryTreeNode(2, BinaryTreeNode(None), BinaryTreeNode(1, BinaryTreeNode(401, BinaryTreeNode(None), BinaryTreeNode(641)), BinaryTreeNode(257))), BinaryTreeNode(271, BinaryTreeNode(None), BinaryTreeNode(28))))
 #b = BinaryTreeNode(1, BinaryTreeNode(2, BinaryTreeNode(4), BinaryTreeNode(5)), BinaryTreeNode(3, BinaryTreeNode(6), BinaryTreeNode(7)))
 
@@ -17,27 +14,32 @@ from Binary_Trees_node import BinaryTreeNode
 def binary_tree_from_preorder_inorder(preorder, inorder):
 
     node_to_inorder_idx = {data: i for i, data in enumerate(inorder)}
+    print("node_to_inorder_idx: ", node_to_inorder_idx)
 
-    # Builds the subtree with preorder[preorder_start:preorder_end] and
-    # inorder[inorder_start:inorder_end].
     def binary_tree_from_preorder_inorder_helper(preorder_start, preorder_end, inorder_start, inorder_end):
+        #print("preorder_start: ", inorder_end)
 
         if preorder_end <= preorder_start or inorder_end <= inorder_start:
+            #print("hello")
             return None
 
         root_inorder_idx = node_to_inorder_idx[preorder[preorder_start]]
+        print("root_inorder_idx: ", root_inorder_idx)
+        #print(root_inorder_idx)
+
         left_subtree_size = root_inorder_idx - inorder_start
 
         return BinaryTreeNode(
             preorder[preorder_start],
+
+
             # Recursively builds the left subtree.
-            binary_tree_from_preorder_inorder_helper(
-                preorder_start + 1, preorder_start + 1 + left_subtree_size,
-                inorder_start, root_inorder_idx),
+            binary_tree_from_preorder_inorder_helper(preorder_start + 1, preorder_start + 1 + left_subtree_size, inorder_start, root_inorder_idx),
+
+
             # Recursively builds the right subtree.
-            binary_tree_from_preorder_inorder_helper(
-                preorder_start + 1 + left_subtree_size, preorder_end,
-                root_inorder_idx + 1, inorder_end))
+            binary_tree_from_preorder_inorder_helper(preorder_start + 1 + left_subtree_size, preorder_end, root_inorder_idx + 1, inorder_end))
+
 
     return binary_tree_from_preorder_inorder_helper(0, len(preorder), 0, len(inorder))
 
