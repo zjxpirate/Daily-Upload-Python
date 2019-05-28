ddde8239a84408565c06d486b2f5e2b5f779d2f8
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-05-28 11:30:06 -0700

    generate first f a b sqrt2 1

---
+++ binary_search_trees/generate_first_k_a_b_sqrt2_1.py
@@ -0,0 +1,93 @@
+
+from bstNode import BstNode, insert
+
+tree_1 = BstNode(19, None, None)
+
+insert(7, tree_1)
+insert(43, tree_1)
+insert(3, tree_1)
+insert(11, tree_1)
+insert(23, tree_1)
+insert(47, tree_1)
+insert(2, tree_1)
+insert(5, tree_1)
+insert(17, tree_1)
+insert(37, tree_1)
+insert(53, tree_1)
+insert(13, tree_1)
+insert(29, tree_1)
+insert(41, tree_1)
+insert(31, tree_1)
+
+
+
+# 10. enumerate numbers of the form a+b*(2)**(1/2)
+
+import math, bintrees
+
+class Number:
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+        self.val = a + b * math.sqrt(2)
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+
+def generate_first_k_a_b_sqrt2(k):
+
+    # initial for 0 + 0 * sqrt(2).
+    candidates = bintrees.RBTree([(Number(0, 0), None)])
+
+    result = []
+    while len(result) < k:
+        next_smallest =candidates.pop_min()[0]
+
+        print("candidates: ", candidates)
+        print("next_smallest: ", next_smallest.val)
+
+        result.append(next_smallest.val)
+        # adds the next two numbers derived from next_smallest.
+        candidates[Number(next_smallest.a + 1, next_smallest.b)] = None
+        candidates[Number(next_smallest.a, next_smallest.b + 1)] = None
+
+    return result
+
+print(generate_first_k_a_b_sqrt2(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
