b1fd66bc8a75e4dd3e21b247bd973136384be2e3
Jiaxing Zhang <j_zhang21@u.pacific.edu>
2019-06-19 20:34:43 -0700

    change making

---
+++ greedy algorithm and invariants/change_making.py
@@ -0,0 +1,16 @@
+
+
+# 1. greedy algorithm boot camp
+
+def change_making(cents):
+    COINS = [100, 50, 25, 10, 5, 1]
+    num_coins = 0
+    for coin in COINS:
+        num_coins += cents // coin
+        cents %= coin
+    return num_coins
+
+
+print(change_making(49))
+
+
